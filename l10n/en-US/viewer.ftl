vlasta-dezelak/pdf-viewer_2

[Editor] Add the possibility to add Popup annotations (bug 1976724)
It's a first big step.
This patch contains a new modal dialog to let the user edit/update a Popup

l10n/en-US/viewer.ftl
@@ -638,3 +638,24 @@ pdfjs-editor-edit-signature-dialog-title = Edit description
 ## Dialog buttons
 
 pdfjs-editor-edit-signature-update-button = Update
+
+##  Edit a comment dialog
+
+pdfjs-editor-comment-actions-button-label = Actions
+pdfjs-editor-comment-actions-button =
+    .title = Actions
+pdfjs-editor-comment-close-button-label = Close
+pdfjs-editor-comment-close-button =
+    .title = Close
+pdfjs-editor-comment-actions-edit-button-label = Edit
+pdfjs-editor-comment-actions-delete-button-label = Delete
+pdfjs-editor-comment-manager-text-input =
+    .placeholder = Enter your comment
+
+pdfjs-editor-edit-comment-manager-cancel-button = Cancel
+pdfjs-editor-edit-comment-manager-save-button = Save
+
+## Edit a comment button in the editor toolbar
+
+pdfjs-editor-comment-button =
+    .title = Edit comment

nicolo-ribaudo/pdf.js

l10n/en-US/viewer.ftl
@@ -644,3 +644,24 @@ pdfjs-editor-edit-signature-dialog-title = Edit description
 ## Dialog buttons
 
 pdfjs-editor-edit-signature-update-button = Update
+
+##  Edit a comment dialog
+
+pdfjs-editor-edit-comment-actions-button-label = Actions
+pdfjs-editor-edit-comment-actions-button =
+    .title = Actions
+pdfjs-editor-edit-comment-close-button-label = Close
+pdfjs-editor-edit-comment-close-button =
+    .title = Close
+pdfjs-editor-edit-comment-actions-edit-button-label = Edit
+pdfjs-editor-edit-comment-actions-delete-button-label = Delete
+pdfjs-editor-edit-comment-manager-text-input =
+    .placeholder = Enter your comment
+
+pdfjs-editor-edit-comment-manager-cancel-button = Cancel
+pdfjs-editor-edit-comment-manager-save-button = Save
+
+## Edit a comment button in the editor toolbar
+
+pdfjs-editor-edit-comment-button =
+    .title = Edit comment